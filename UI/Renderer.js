c91b2a04c3c1dd4d5c288479d68007641f604367
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2015-01-21 23:06:06 +0200

    converted Logic classes into modules

--- UI/Renderer.js
+++ UI/Renderer.js
@@ -1,10 +1,13 @@
 /// <reference path="../Logic/BlockType.js" />
 
 'use strict';
- var SnakeNS = SnakeNS || {};
+$(document).ready(function() {
+	var SnakeNS = SnakeNS || {};
 
-module.exports = function (classNames) {
-	var renderField = function (field) {
+	var socket = io();
+
+	socket.on('render field', function(CLASS_NAMES, field) {
+		console.log(CLASS_NAMES);
 		var table = document.createElement("table"),
 			row,
 			cell,
@@ -16,43 +19,56 @@ module.exports = function (classNames) {
 
 		for (i = 0; i < field.length; i++) {
 			row = document.createElement('tr');
-				for (j = 0; j < field[i].length; j++) {
-					cell = document.createElement('td');
-					cell.className = this.CLASS_NAMES[field[i][j]];
-					cell.id = 'block-' + j + '-' + i;
-					row.appendChild(cell);
-				}
+			for (j = 0; j < field[i].length; j++) {
+				cell = document.createElement('td');
+				cell.className = CLASS_NAMES[field[i][j]];
+				cell.id = 'block-' + j + '-' + i;
+				row.appendChild(cell);
+			}
 
-		table.appendChild(row);
+			table.appendChild(row);
 		}
-	},
+	});
 
-	renderBlock = function (block, blockType) {
-		var cell = document.getElementById('block-'+ block.x + '-' + block.y);
-		cell.className = this.CLASS_NAMES[blockType];
-	},
-
-	renderPoints = function (points) {
+	socket.on('render points', function(points) {
 		var pointsDiv = document.createElement("div");
 		pointsDiv.id = 'points';
 		pointsDiv.innerHTML = "Points: " + points;
 		document.body.appendChild(pointsDiv);
-	},
+	});
 
-	updatePoints = function (points) {
-		document.getElementById('points').innerHTML = "Points: " + points;
-	},
+	// module.exports = function(classNames) {
+	// 	var renderField = function(field) {
 
-	eraseBlock = function (block) {
-		var cell = document.getElementById('block-'+ block.x + '-' + block.y);
-		cell.className = this.CLASS_NAMES[SnakeNS.BLOCK_TYPE.BLANK];
-	}
+	// 		},
 
-	return {
-		renderField: renderField,
-		eraseBlock: eraseBlock,
-		renderBlock: renderBlock,
-		renderPoints: renderPoints,
-		updatePoints: updatePoints
-	};
-};
+	// 		renderBlock = function(block, blockType) {
+	// 			var cell = document.getElementById('block-' + block.x + '-' + block.y);
+	// 			cell.className = this.CLASS_NAMES[blockType];
+	// 		},
+
+	// 		renderPoints = function(points) {
+	// 			var pointsDiv = document.createElement("div");
+	// 			pointsDiv.id = 'points';
+	// 			pointsDiv.innerHTML = "Points: " + points;
+	// 			document.body.appendChild(pointsDiv);
+	// 		},
+
+	// 		updatePoints = function(points) {
+	// 			document.getElementById('points').innerHTML = "Points: " + points;
+	// 		},
+
+	// 		eraseBlock = function(block) {
+	// 			var cell = document.getElementById('block-' + block.x + '-' + block.y);
+	// 			cell.className = this.CLASS_NAMES[SnakeNS.BLOCK_TYPE.BLANK];
+	// 		}
+
+	// 	return {
+	// 		renderField: renderField,
+	// 		eraseBlock: eraseBlock,
+	// 		renderBlock: renderBlock,
+	// 		renderPoints: renderPoints,
+	// 		updatePoints: updatePoints
+	// 	};
+	// };
+});
